e85b3a267b1b5dc241b142f1e7ce13812d049b88
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-03-11 14:48:33 +0000

    start implementing cli api

--- src/bin/dongleExt.ts
+++ src/bin/dongleExt.ts
@@ -1,67 +1,119 @@
 #!/usr/bin/env node
 
+import * as program from "commander";
 import { AmiClient } from "../lib/index";
 import { spawn } from "child_process";
 require("colors");
 
-function isServiceRunning(): Promise<boolean> {
+function assertServiceRunning(): Promise<void> {
 
-    return new Promise<boolean>(resolve => {
+    return new Promise<void>(resolve => {
         spawn("systemctl", ["status", "dongleExt.service"])
             .stdout
             .once("data",
             data => {
 
-                let line= data.toString("utf8").split("\n")[2];
+                let line = data.toString("utf8").split("\n")[2];
 
-                resolve( line && line.match(/^\ *Active:\ *active/) );
+                if( !line || !line.match(/^\ *Active:\ *active/) ){
+                    console.log("Error: dongleExt service is not running!".red);
+                    console.log("run: sudo systemctl start dongleExt");
+                    process.exit(-1);
+                }
+
+                resolve();
             }
             );
     });
 
 }
 
+program
+    .version('0.0.1')
 
-(async ()=> {
+program
+    .command("active")
+    .alias("a")
+    .description("List PIN locked dongle's IMEI")
+    .option("-j, --json", "Format result in json")
+    .action(options => {
+        (async () => {
 
+            await assertServiceRunning();
 
-    if( !await isServiceRunning() ){
-        console.log("Service not running");
-        process.exit(1);
-    }
+            let client = AmiClient.getLocal();
 
-    console.log("poccess running");
+            let dongles = await new Promise<string[]>(resolve => client.getActiveDongles(resolve));
 
+            if (!options.json)
+                for (let imei of dongles)
+                    console.log(imei);
+            else
+                console.log(JSON.stringify(dongles, null, 2));
 
-
-
-})();
-
-
-/*
-console.log("Hello world");
-
-console.log(process.argv);
-
-process.exit(0);
-
-
-let command= process.argv[1];
-
-
-let amiClient= AmiClient.getLocal();
-
-switch( command ){
-    case "GetActiveDongles": 
-        amiClient.getActiveDongles( dongles => {
-            console.log(JSON.stringify(dongles, null, 2));
             process.exit(0);
-        });
-    break;
-}
-*/
+
+        })();
+    });
+
+program
+    .command("send")
+    .alias("s")
+    .description("Send SMS")
+    .option("-i, --imei [imei]", "IMEI of the dongle")
+    .option("-n, --number [number]", "target phone number")
+    .option("-t, --text [text]", "Text of the message")
+    .action(options => {
+        (async () => {
+
+            await assertServiceRunning();
+
+            let { imei, number, text } = options;
+
+            if (!imei || !number || !text) {
+                console.log("Error: command malformed".red);
+                console.log(options.optionHelp());
+                process.exit(-1);
+            }
+
+            text= JSON.parse('"'+text+'"');
+
+            let client = AmiClient.getLocal();
+
+            client.sendMessage(imei, number, text, (error, messageId)=> {
+
+                if( error ){
+                    console.log(error.message.red);
+                    process.exit(-1);
+                }
+
+                console.log(messageId);
+
+                process.exit(0);
+
+            });
+
+
+        })();
+    });
+
+
+program.parse(process.argv);
 
 
 
 
 
+    /*
+    program
+      .version('0.0.1')
+      .command("locked", "List PIN locked dongle's IMEI").alias("l")
+      .command("active", "List active dongle's IMEI").alias("a")
+      .command("select", "Select a dongle for the subsequent calls")
+      .command("unlock", "Provide pin or puk to unlock dongle")
+      .command("send", "Send SMS message").alias("s")
+      .command("contacts", "Retrieve contact stored in a SIM card phonebook memory").alias("c")
+      .command("create-contact", "Add a new contact in the phonebook memory")
+      .command("delete-contact", "Add a new contact in the phonebook memory")
+      .parse(process.argv);
+    */
