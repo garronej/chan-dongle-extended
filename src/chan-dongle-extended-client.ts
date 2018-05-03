b80001ed4db6ddc6da632da912b24ce05722ae41
garronej <joseph.garrone.gj@gmail.com>
2018-05-03 23:08:35 +0000

    switching to dist

--- src/chan-dongle-extended-client.ts
+++ src/chan-dongle-extended-client.ts
@@ -1,8 +1,8 @@
 
 // For dist replace ../../chan-dongle-extended-client by chan-dongle-extended-client
 
-import * as apiDeclaration from "../../chan-dongle-extended-client/dist/lib/apiDeclaration";
-import * as misc from "../../chan-dongle-extended-client/dist/lib/misc";
-import { types, DongleController } from  "../../chan-dongle-extended-client";
+import * as apiDeclaration from "chan-dongle-extended-client/dist/lib/apiDeclaration";
+import * as misc from "chan-dongle-extended-client/dist/lib/misc";
+import { types, DongleController } from  "chan-dongle-extended-client";
 
 export { types, apiDeclaration, misc, DongleController };
