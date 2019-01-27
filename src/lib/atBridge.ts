7be5d27e134688bdcff5532333f74b0556cfd114
garronej <joseph.garrone.gj@gmail.com>
2019-01-27 23:13:46 +0100

    increase open delay

--- src/lib/atBridge.ts
+++ src/lib/atBridge.ts
@@ -225,7 +225,8 @@ function atBridge(
 
                 debug(`(AT) urc: ${readableAt(urc.raw)} ( ${doNotForward ? "NOT forwarded" : "forwarded"} to asterisk-chan-dongle )`);
 
-            })
+            }
+        )
     );
 
 
